ae31600e63d8b5d7da367bf46638771e54a09b99
Gulin Maksim <painbb@ya.ru>
2016-09-26 09:10:43 +0300

    adding new class localc

---
+++ src/main/java/tu/test/localc.java
@@ -0,0 +1,27 @@
+package tu.test;
+
+/**
+ * Created by gulin_ma on 26.09.16.
+ */
+public class localc {
+
+
+    private int a;
+    private String s;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+}
